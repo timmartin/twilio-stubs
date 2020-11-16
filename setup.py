6cfc2e4e763232381bf5b0c9eedd15722eac0c5c
Tim Martin <tim@asymptotic.co.uk>
2020-11-16 13:48:10 +0000

    Automatically deploy to PyPI from CI

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def find_stubs(package):
 
 setuptools.setup(
     name="twilio-stubs",
-    version="0.0.2",
+    version="0.0.3",
     author="Tim Martin",
     author_email="tim@asymptotic.co.uk",
     description="Type declarations for the Twilio API",
